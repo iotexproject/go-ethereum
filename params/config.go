f9f8dc119f999bdbda3330de64e7e58322d96bb3
Dustin Xie <dustin.xie@iotex.io>
2019-07-23 15:12:11 -0700

    adjust CREATE2 gas for bering height

--- params/config.go
+++ params/config.go
@@ -292,6 +292,7 @@ var (
 		TerminalTotalDifficultyPassed: false,
 		Ethash:                        new(EthashConfig),
 		Clique:                        nil,
+		BeringBlock:                   big.NewInt(0),
 	}
 
 	// AllCliqueProtocolChanges contains every protocol change (EIPs) introduced
@@ -322,6 +323,7 @@ var (
 		TerminalTotalDifficultyPassed: false,
 		Ethash:                        nil,
 		Clique:                        &CliqueConfig{Period: 0, Epoch: 30000},
+		BeringBlock:                   big.NewInt(0),
 	}
 
 	// TestChainConfig contains every protocol change (EIPs) introduced
@@ -352,6 +354,7 @@ var (
 		TerminalTotalDifficultyPassed: false,
 		Ethash:                        new(EthashConfig),
 		Clique:                        nil,
+		BeringBlock:                   big.NewInt(0),
 	}
 
 	// NonActivatedConfig defines the chain configuration without activating
@@ -382,6 +385,7 @@ var (
 		TerminalTotalDifficultyPassed: false,
 		Ethash:                        new(EthashConfig),
 		Clique:                        nil,
+		BeringBlock:                   nil,
 	}
 	TestRules = TestChainConfig.Rules(new(big.Int), false, 0)
 )
@@ -492,6 +496,9 @@ type ChainConfig struct {
 	// Various consensus engines
 	Ethash *EthashConfig `json:"ethash,omitempty"`
 	Clique *CliqueConfig `json:"clique,omitempty"`
+
+	// The following are the iotex configs
+	BeringBlock *big.Int `json:"beringBlock,omitempty"` // Bering switch block that ignores CREATE2 gas
 }
 
 // EthashConfig is the consensus engine configs for proof-of-work based sealing.
@@ -695,6 +702,11 @@ func (c *ChainConfig) IsCancun(time uint64) bool {
 // IsPrague returns whether num is either equal to the Prague fork time or greater.
 func (c *ChainConfig) IsPrague(time uint64) bool {
 	return isTimestampForked(c.PragueTime, time)
+}
+
+// IsBering returns whether num represents a block number after the Bering block
+func (c *ChainConfig) IsBering(num *big.Int) bool {
+	return isForked(c.BeringBlock, num)
 }
 
 // CheckCompatible checks whether scheduled fork transitions have been imported
